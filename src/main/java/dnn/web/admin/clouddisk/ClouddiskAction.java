4692b1e7b7cf769564823b989d8bc867e4843778
huanghuanlai <102535481@qq.com>
2016-07-02 16:50:22 +0800

    优化代码

--- src/main/java/dnn/web/admin/clouddisk/ClouddiskAction.java
+++ src/main/java/dnn/web/admin/clouddisk/ClouddiskAction.java
@@ -1,7 +1,6 @@
 package dnn.web.admin.clouddisk;
 
 import com.dounine.clouddisk360.parser.deserializer.login.LoginUserToken;
-import com.dounine.clouddisk360.pool.PoolingHttpClientConnection;
 import com.dounine.clouddisk360.store.BasePathCommon;
 import com.dounine.clouddisk360.util.MD5Util;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,7 +12,7 @@ public class ClouddiskAction {
 
 	public static final LoginUserToken LOGIN_USER_TOKEN =
 			new LoginUserToken("403833139@qq.com",
-			"d438f8205208b128ec70160af617da21");
+			"");
 
 	public static void main(String[] args){
 		System.out.println(MD5Util.MD5("987654321.0"));
@@ -21,7 +20,7 @@ public class ClouddiskAction {
 
 	static {
 		BasePathCommon.BASE_PATH = "/Users/huanghuanlai/Desktop/clouddisk/";
-		PoolingHttpClientConnection.initHttpClientConnectionManager();
+		//PoolingHttpClientConnection.initHttpClientConnectionManager();
 	}
 
 }
